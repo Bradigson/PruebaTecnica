83c53d6912edc52d2e652990ad9271d59e427ca5
Bradigson <71954800+Bradigson@users.noreply.github.com>
2023-03-06 19:51:03 -0400

    Update Routers.tsx

--- Frontend/aseguradora/src/routes/Routers.tsx
+++ Frontend/aseguradora/src/routes/Routers.tsx
@@ -2,15 +2,17 @@ import {Routes, Route} from 'react-router-dom'
 import Form from '../components/Form';
 import Home from '../components/Home';
 import Login from '../components/Login';
-
+import { useState, useEffect } from 'react';
 
 
 const Rutas = ()=>{
+     const [takeToken, setTakeToken] = useState<string>("");
+    console.log(`${"Rutas component"} ${takeToken}`);
     return(
         <Routes>
-            <Route path="/" element={<Login/>}/>
-            <Route path="home" element={<Home/>}/>
-            <Route path="create" element={<Form/>}/>
+            <Route path="/" element={<Login token={setTakeToken}  onClick={(message : string)=>{console.log(message); setTakeToken(message);}}/>}/>
+            <Route path="home" element={<Home tokenURL={takeToken}/>}/>
+            <Route path="create" element={<Form tokenURL={takeToken}/>}/>
         </Routes>
 
     )
